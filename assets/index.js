13cded7fa171e0204ac1d43656c4220cc7a1b920
Justuks <justinas.valutis@gmail.com>
2022-01-31 16:40:44 +0000

    import a font from google fonts

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,4 @@
 document.getElementById('press').onclick = function(hide){
     document.getElementById('header-text').innerHTML = 'Please, complete your test below.'
+    document.getElementById('second-paragraph').style.display = 'none'
 }

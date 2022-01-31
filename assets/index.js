cc630c42f65d521f702c51fff691411143d1a8f8
Justuks <justinas.valutis@gmail.com>
2022-01-31 14:23:56 +0000

    add few bits on header paragraph, start to make ribs for the competency test

--- assets/index.js
+++ assets/index.js
@@ -1,3 +1,3 @@
 document.getElementById('press').onclick = function(hide){
-    document.getElementById('header-text').innerHTML = 'Please, complete your test below'
+    document.getElementById('header-text').innerHTML = 'Please, complete your test below.'
 }

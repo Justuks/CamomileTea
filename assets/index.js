299485a4abdd751f738fecc2ead787723065ed40
Justuks <justinas.valutis@gmail.com>
2022-02-01 10:07:57 +0000

    will start to style game container

--- assets/index.js
+++ assets/index.js
@@ -1,4 +1,5 @@
-document.getElementById('press').onclick = function(hide){
+document.getElementById('press').onclick = function(){
     document.getElementById('header-text').innerHTML = 'Please, complete your test below.'
     document.getElementById('second-paragraph').style.display = 'none'
+    document.getElementById('game').style.visibility = 'hidden'
 }

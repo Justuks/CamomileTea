bb431be7718bfcce759ea0511ff1eb780c2f434f
Justuks <justinas.valutis@gmail.com>
2022-02-02 12:36:25 +0000

    working ona game in js

--- assets/index.js
+++ assets/index.js
@@ -2,3 +2,11 @@ document.getElementById('press').onclick = function(){
     document.getElementById('header-text').innerHTML = 'Please, complete your test below.'
     document.getElementById('game-container').style.visibility = 'visible'
 }
+//starting to arrange variables for the game
+let aVariant = document.getElementById('a')
+let bVariant = document.getElementById('b')
+let cVariant = document.getElementById('c')
+let nextBtn = document.getElementById('next')
+let prevBtn = document.getElementById('previous')
+let resetBtn = document.getElementById('reset')
+let subBtn = document.getElementById('submit')

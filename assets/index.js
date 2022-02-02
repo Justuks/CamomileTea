836feae5657a8accbccf2247e5484053d62d01d3
Justuks <justinas.valutis@gmail.com>
2022-02-02 18:10:15 +0000

    continue to work on game function

--- assets/index.js
+++ assets/index.js
@@ -7,3 +7,26 @@ document.getElementById('press').onclick = function () {
 
 //starting to arrange variables for the game form
 
+let question1 = document.getElementById('first-form').q1.value
+let question2 = document.getElementById('first-form').q2.value
+let question3 = document.getElementById('first-form').q3.value
+let count = 0
+
+function result(){
+    if(question1 == 'c'){
+        count++;
+    }
+    if(question2 == 'b'){
+        count++;
+    }
+    if(question3 == 'c'){
+        count++;
+    }
+    if (count == 3){
+        document.getElementById('header-text').innerHTML = 'Congratulations, turn on ignition and input required data.'
+    }
+    else{
+        document.getElementById('header-text').innerHTML = 'No succsess, back to the drawing board.'
+    }
+
+}

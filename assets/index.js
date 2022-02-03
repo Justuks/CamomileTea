d97f89a3bc1c6386dcf496cc262b2382c4b4e3e4
Justuks <justinas.valutis@gmail.com>
2022-02-03 18:57:16 +0000

    struggle with form connection

--- assets/index.js
+++ assets/index.js
@@ -7,26 +7,26 @@ document.getElementById('press').onclick = function () {
 
 //starting to arrange variables for the game form
 
-let question1 = document.getElementById('1').value
-let question2 = document.getElementById('2').value
-let question3 = document.getElementById('3').value
+let question1 = 'Port'
+let question2 = 1.85
+let question3 = 360
 let count = 0
+console.log(question1)
 
-function result(){
-    if(question1 == 'c'){
+function outcome() {
+    if (question1 == 'Port') {
         count++;
     }
-    if(question2 == 'b'){
+    if (question2 == 1.85) {
+        count++;
+    }
+    if (question3 == 360) {
         count++;
     }
     console.log(count)
-    if(question3 == 'a'){
-        count++;
-    }
-    if (count == 3){
+    if (count === 3) {
         document.getElementById('header-text').innerHTML = 'Congratulations, turn on ignition and input required data.'
-    }
-    else{
+    } else {
         document.getElementById('header-text').innerHTML = 'No succsess, back to the drawing board.'
     }
 
